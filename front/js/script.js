1b568830176d973a413b388a2a7948cddb04378f
Briar-Bear <ed.taylor04@yahoo.co.uk>
2022-09-20 15:06:02 +0100

    started fetching products through JS

---
+++ front/js/script.js
@@ -0,0 +1,8 @@
+const { response } = require("../../back/app")
+
+fetch('http://localhost:3000/api/products')
+.then(response => console.log(response))
+
+
+
+http://localhost:3000/api/products/images/kanap01.jpeg
