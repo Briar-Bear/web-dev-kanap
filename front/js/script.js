a7bfeb1bffc2285d982d96374db0567bf5df09ac
Briar-Bear <ed.taylor04@yahoo.co.uk>
2022-10-04 11:10:57 +0100

    created a promise on script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,8 +1,7 @@
-const { response } = require("../../back/app")
+
 
 fetch('http://localhost:3000/api/products')
-.then(response => console.log(response))
+.then((response) => response.json())
+.then((data) => console.log(data));
 
 
-
-http://localhost:3000/api/products/images/kanap01.jpeg
