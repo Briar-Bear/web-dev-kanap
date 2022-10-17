ea74f70de42241d2cdda30945553d51745629cd8
Briar-Bear <ed.taylor04@yahoo.co.uk>
2022-10-17 15:18:29 +0100

    addied code to fetch the products to the page

---
+++ front/js/product.js
@@ -0,0 +1,15 @@
+const params = new URLSearchParams(window.location.search);
+const productId = params.get('id')
+
+window.fetch('http://localhost:3000/api/products/' + productId)
+.then (response => response.json())
+.then(product => {
+    const imgContainer = document.querySelector('.item__img')
+    const title = document.querySelector('#title')
+    const price = document.querySelector('#price')
+
+    imgContainer.innerHTML = `<img src="${product.imageURL}" alt="${product.description}">`
+    title.textContent = product.name
+}).catch((error) => {
+    console.log(error);
+})
