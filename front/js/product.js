948df1fbca95754205f36a020bbacf548484b0e0
Briar-Bear <ed.taylor04@yahoo.co.uk>
2022-10-24 15:48:36 +0100

    fetched the description and price id's from back

--- front/js/product.js
+++ front/js/product.js
@@ -9,12 +9,28 @@ window.fetch('http://localhost:3000/api/products/' + productId)
     const imgContainer = document.querySelector('.item__img')
     const title = document.querySelector('#title')
     const price = document.querySelector('#price')
+    const selectColor = document.querySelector('#color')
+    const description = document.querySelector('#description')
 
+    price.innerHTML = `${product.price}`
+    description.innerHTML =  `${product.description}`
     imgContainer.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
     title.textContent = product.name
 }).catch((error) => {
     console.log(error);
+
+    for (let i = 0; i < product.colors.length; i++) {
+        const color = product.colors[i];
+        const option = document.createElement('option')
+
+        option.setAttribute('value', color)
+        option.textContent = color
+
+        selectColor.appendChild(option)
+    }
 })
+
+
 
 
 
